fa0ee9279278250f56ed1750d8dcae1c30358be9
adams77 <jingyinling@foxmail.com>
2026-04-01 16:35:55 +0800

    modify api/auth.js的client_id

--- api/auth.js
+++ api/auth.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
-  const client_id = process.env.OAUTH_CLIENT_ID;
+  const client_id = process.env.GITHUB_CLIENT_ID;
   const redirect_uri = `https://${req.headers.host}/api/callback`;
   const url = `https://github.com/login/oauth/authorize?client_id=${client_id}&scope=repo,user&redirect_uri=${redirect_uri}`;
   res.redirect(url);
